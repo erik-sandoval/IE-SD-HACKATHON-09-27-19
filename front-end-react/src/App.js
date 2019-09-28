5011fc69b7440474a0effbf59a70179fcb7cd47e
erik sandoval <erik.sandoval@icloud.com>
2019-09-28 15:54:33 -0700

    added test url for github api.

--- front-end-react/src/App.js
+++ front-end-react/src/App.js
@@ -1,10 +1,18 @@
 import React from "../node_modules/react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import GithubApi from "./Components/GithubApi";
+
+
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'></header>
+      <Router>
+
+        <Route path="/github-api" component={GithubApi}/>
+      </Router>
     </div>
   );
 }
---
+++ front-end-react/src/Components/GithubApi.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+import React, {useState, useEffect} from 'react';
+
+const GithubApi = () => {
+const [data, setData] = useState([])
+
+useEffect(() => {
+
+})
+
+return <div>hello</div>
+}
+ 
+export default GithubApi;
