3a8853673f32f066b6de91950e0eb2e06b0f3ec8
erik sandoval <erik.sandoval@icloud.com>
2019-09-28 17:25:02 -0700

    merged and fixed errors

--- front-end-react/src/App.js
+++ front-end-react/src/App.js
@@ -2,12 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Profile from "./Components/Profile";
-
-
-
 import Error from './Pages/Error';
-
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 function App() {
   return (
@@ -15,6 +10,7 @@ function App() {
       <header className='App-header'></header>
       <Router>
         <Route path="/profile" component={Profile}/>
+        <Route path="/error" component={Error} />
       </Router>
     </div>
   );
